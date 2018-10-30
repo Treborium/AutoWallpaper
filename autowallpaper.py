0e17aebe26f42428b101d52945c60e032c19a7a4
Robert Fuchs <robertfuchs97@gmail.com>
2018-10-30 20:58:39 +0100

    add possibility to specify search terms

--- autowallpaper.py
+++ autowallpaper.py
@@ -6,27 +6,33 @@
 
 import os
 import subprocess
+import sys
 import urllib.request
 
-URL = "https://source.unsplash.com/featured/2556x1600?landscape"
+URL = "https://source.unsplash.com/featured/2556x1600"
 WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 
-def download_image(url):
+def download_image(url, query_params):
     file_name = "image.jpg"
-    urllib.request.urlretrieve(url, WORKING_DIRECTORY + "/" + file_name)
+    urllib.request.urlretrieve(
+        f"{url}?{query_params}", WORKING_DIRECTORY + "/" + file_name)
     return file_name
 
 
 def set_image_as_wallpaper(file_name):
-    subprocess.run(f"gsettings set org.gnome.desktop.background picture-uri file:///{WORKING_DIRECTORY}/{file_name}".split(" "))
+    subprocess.run(
+        f"gsettings set org.gnome.desktop.background picture-uri file:///{WORKING_DIRECTORY}/{file_name}".split(" "))
     print("Wallpaper changed!")
 
 
-def main():
-    file_name = download_image(URL)
+def main(args):
+    file_name = download_image(URL, query_params)
     set_image_as_wallpaper(file_name)
 
 
 if __name__ == '__main__':
-    main()
+    query_params = "landscape"
+    if len(sys.argv) > 1:
+        query_params = sys.argv[1]
+    main(query_params)
